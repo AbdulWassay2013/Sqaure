556143b0aed06ee6821afca038cde7df33748524
AbdulWassay2013 <nadeemalbaraka@gmail.com>
2025-09-16 15:12:42 +0500

    Square In Python

---
+++ Square.py
@@ -0,0 +1,9 @@
+n = int(input("Please Enter A Number "))
+def square(n):
+    return n*n
+def two(number):
+    if number % 2 == 0:
+        return square(n)
+    else:
+        return False
+print(two(n))
